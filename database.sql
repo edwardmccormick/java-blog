a3e1de279d988febfed728d1e74e70dc5f94525d
Ted McCormick <ted.mccormick@gmail.com>
2021-03-15 21:03:56 -0500

    Updated data base?

---
+++ database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS 'blog_db'.'blog' (
+     'id' INT UNSIGNED NOT NULL,
+     'title' VARCHAR(255) NULL,
+     'body' TEXT NULL,
+     'keywords' VARCHAR(255) NULL,
+     PRIMARY KEY ('id'));
